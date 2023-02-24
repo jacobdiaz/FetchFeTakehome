03ec38b685ded74a03f3d0496855fdb8e8fa2d2e
Jacob Diaz <jacobdiaz@Jacobs-MacBook-Pro.local>
2023-02-23 18:35:23 -0600

    [feat] Create alert notification

---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import Input from "../components/Input";
+import { useForm } from "../hooks";
+import { welcomeText } from "../utils";
+import { FORM_API } from "../api";
+import axios from "axios";
+import { State } from "../types";
+import Dropdown from "../components/Dropdown";
+import bcrypt from "bcryptjs-react";
+
+type Props = {
+  showAlert3s: () => void;
+  setAlertData: (data: Object) => void;
+};
+
+const LoginForm = ({ showAlert3s, setAlertData }: Props) => {
+  const handleSubmit = async () => {
+    var encryptedPassword = await bcrypt.hash(values.Password, 10);
+    await axios
+      .post(FORM_API, {
+        name: values.Name,
+        email: values.Email,
+        password: encryptedPassword,
+        occupation: values.Occupation,
+        state: values.State,
+      })
+      .then((res) => console.log(res))
+      .catch((err) => {});
+  };
+
+  const [stateOptions, setStateOptions] = useState<State[]>([]);
+  const [occupationOptions, setOccupationOptions] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const { values, onChange, onSubmit } = useForm(handleSubmit, {
+    Email: "",
+    Name: "",
+    Password: "",
+    Occupation: "",
+    State: {},
+    hasValidInputs: false,
+  });
+
+  useEffect(() => {
+    async function fetchFormOptions() {
+      setIsLoading(true);
+      const { data } = await axios.get(FORM_API);
+      const { states, occupations } = data;
+      setStateOptions(states);
+      setOccupationOptions(occupations);
+      setIsLoading(false);
+    }
+    fetchFormOptions();
+  }, []);
+
+  return (
+    <div className='flex justify-center'>
+      {isLoading ? (
+        <div>Loading</div>
+      ) : (
+        <form
+          className=' card w-3/6 bg-base-200 shadow-xl p-12 '
+          onSubmit={onSubmit}
+        >
+          {<h1 className='font-black text-2xl'>Sign In</h1>}
+          <p className='my-6'>{welcomeText}</p>
+          <Input varient='Email' onChange={onChange} />
+          <Input varient='Name' onChange={onChange} />
+          <Input varient='Password' onChange={onChange} />
+
+          <div className='flex row'>
+            <Dropdown
+              options={occupationOptions}
+              placeholder='Occupation'
+              onChange={onChange}
+            />
+            <Dropdown
+              options={stateOptions}
+              placeholder='State'
+              onChange={onChange}
+            />
+          </div>
+
+          <div
+            className='tooltip tooltip-bottom'
+            data-tip={
+              !values.hasValidInputs ? "Please Insert Valid Inputs" : ""
+            }
+          >
+            <button
+              className='btn btn-primary w-full mt-8 disabled:opacity-40 disabled:bg-primary disabled:text-white'
+              type='submit'
+              disabled={!values.hasValidInputs}
+            >
+              Sign In
+            </button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
+
+export default LoginForm;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,97 +1,23 @@
 import { useEffect, useState } from "react";
-import Input from "../components/Input";
-import { useForm } from "../hooks";
-import { welcomeText } from "../utils";
-import { FORM_API } from "../api";
-import axios from "axios";
-import { State } from "../types";
-import Dropdown from "../components/Dropdown";
+import Alert from "../components/Alert";
+import LoginForm from "../components/LoginForm";
 
 const Login = () => {
-  const handleSubmit = async () => {
-    const { data } = await axios.post(FORM_API, {
-      name: values.Name,
-      email: values.Email,
-      password: values.Password,
-      occupation: values.Occupation,
-      state: values.State,
-    });
-    console.log(data);
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertData, setAlertData] = useState({});
+
+  const showAlert3s = () => {
+    setShowAlert(true);
+    setTimeout(() => setShowAlert(false), 3000);
   };
 
-  const [stateOptions, setStateOptions] = useState<State[]>([]);
-  const [occupationOptions, setOccupationOptions] = useState<string[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
-
-  const { values, onChange, onSubmit } = useForm(handleSubmit, {
-    Email: "",
-    Name: "",
-    Password: "",
-    Occupation: "",
-    State: {},
-    hasValidInputs: false,
-  });
-
-  useEffect(() => {
-    // call fetch with Form API
-    async function fetchFormOptions() {
-      setIsLoading(true);
-      const { data } = await axios.get(FORM_API);
-      const { states, occupations } = data;
-      setStateOptions(states);
-      setOccupationOptions(occupations);
-      setIsLoading(false);
-    }
-    fetchFormOptions();
-  }, []);
-
-  //   const { errors, setErrors } = useState([]);
+  useEffect(() => showAlert3s(), []);
 
   return (
-    <>
-      {isLoading ? (
-        <div>Loading</div>
-      ) : (
-        <form
-          className='card w-3/6 bg-base-200 shadow-xl p-12 '
-          onSubmit={onSubmit}
-        >
-          {<h1 className='font-black text-2xl'>Sign In</h1>}
-          <p className='my-6'>{welcomeText}</p>
-          <Input varient='Email' onChange={onChange} />
-          <Input varient='Name' onChange={onChange} />
-          <Input varient='Password' onChange={onChange} />
-
-          <div className='flex row'>
-            <Dropdown
-              options={occupationOptions}
-              placeholder='Occupation'
-              onChange={onChange}
-            />
-            <Dropdown
-              options={stateOptions}
-              placeholder='State'
-              onChange={onChange}
-            />
-          </div>
-
-          <div
-            className='tooltip tooltip-bottom'
-            data-tip={
-              !values.hasValidInputs ? "Please Insert Valid Inputs" : ""
-            }
-          >
-            <button
-              className='btn btn-primary w-full mt-8 disabled:opacity-40 disabled:bg-primary disabled:text-white'
-              type='submit'
-              disabled={!values.hasValidInputs}
-            >
-              Sign In
-            </button>
-          </div>
-        </form>
-      )}
-    </>
+    <div>
+      <LoginForm showAlert3s={showAlert3s} setAlertData={setAlertData} />
+      {showAlert && <Alert data={alertData} />}
+    </div>
   );
 };
 
